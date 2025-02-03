dda43a007d683b2c40f99176dbcafcb5942230df
Medo Abdo <Medoshakir590@gmail.com>
2025-02-03 22:05:55 +0400

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import serverlessExpress from '@vendia/serverless-express'
-
-let server: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.init();
-  const expressApp = app.getHttpAdapter().getInstance();
-  server = serverlessExpress({ app: expressApp });
+  const port = process.env.PORT;
+  await app.listen(port);
 }
-
-export const handler = async (event: any, context: any) => {
-  if (!server) {
-    await bootstrap(); // تأكد من تهيئة السيرفر عند أول استدعاء
-  }
-  return server(event, context);
-};
+export default bootstrap();
