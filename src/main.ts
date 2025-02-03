74f2bc36e79b5c235b269a5a69b7acae6fd08f39
Medo Abdo <Medoshakir590@gmail.com>
2025-02-03 21:37:48 +0400

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// استخدام require بدلاً من import الافتراضي
+const serverlessExpress = require('@vendia/serverless-express');
+
+let server: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  return app;
+  await app.init();
+  const expressApp = app.getHttpAdapter().getInstance();
+  // استخدم serverlessExpress كدالة وتمرير كائن الخيارات مع الخاصية app
+  server = serverlessExpress({ app: expressApp });
 }
+bootstrap();
 
-export default bootstrap()
+export const handler = (event: any, context: any) => {
+  return server(event, context);
+};
